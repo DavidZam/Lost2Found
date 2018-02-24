5b46ceede7268002cf156cd326dc3046a38c88b5
David Zamora <davidzamorarey@gmail.com>
2018-02-24 15:27:09 +0100

    First commit
    first commit for lost2found frontend aplication

---
+++ app/src/main/java/es/lost2found/lost2found/loginregisterUI/LoginActivity.java
@@ -0,0 +1,14 @@
+package es.lost2found.lost2found.loginregisterUI;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import es.lost2found.R;
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/es/lost2found/lost2found/loginregisterUI/RegisterActivity.java
@@ -0,0 +1,4 @@
+package es.lost2found.lost2found.loginregisterUI;
+
+public class RegisterActivity {
+}
