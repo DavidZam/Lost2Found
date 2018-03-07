de7669a52789ca33f9200611aeb2d8bf41a003f0
David Zamora <davidzamorarey@gmail.com>
2018-03-07 20:51:11 +0100

    Views changes
    Changes in Login and Register views.

--- app/src/main/java/es/lost2found/lost2found/loginregisterUI/LoginActivity.java
+++ app/src/main/java/es/lost2found/lost2found/loginregisterUI/LoginActivity.java
@@ -1,7 +1,10 @@
 package es.lost2found.lost2found.loginregisterUI;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
 
 import es.lost2found.R;
 
@@ -11,4 +14,30 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+
+    /**
+     *
+     * @param view
+     */
+    public void register(View view) {
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    /**
+     *
+     * @param view
+     */
+    public void login(View view) {
+        EditText editText = (EditText) findViewById(R.id.email);
+        String email = editText.getText().toString();
+
+        EditText passwordEditText = (EditText) findViewById(R.id.password);
+        String password = passwordEditText.getText().toString();
+
+        //new LoginDB().execute(email, password);
+    }
+
+
 }
