4f51dcfde9fda6536c728a2571a64d5b8991714e
Carolina <carolriv@ucm.es>
2018-03-08 11:43:46 +0100

    New Activity
    Created the new home_activity

---
+++ app/src/main/java/es/lost2found/lost2found/loginregisterUI/HomeActivity.java
@@ -0,0 +1,10 @@
+package es.lost2found.lost2found.loginregisterUI;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Carolina on 08/03/2018.
+ */
+
+public class HomeActivity extends AppCompatActivity {
+}
--- app/src/main/java/es/lost2found/lost2found/loginregisterUI/LoginActivity.java
+++ app/src/main/java/es/lost2found/lost2found/loginregisterUI/LoginActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import es.lost2found.R;
@@ -13,6 +14,8 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
     }
 
     /**
