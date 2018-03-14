71e5551b836d9ec65124b8b70eb2401bcd92e9c1
David Zamora <davidzamorarey@gmail.com>
2018-03-14 14:09:26 +0100

    New ChatActivity
    Created the new ChatActivity and connected with the navigation bar.

--- app/src/main/java/es/lost2found/chatUI/ChatActivity.java
+++ app/src/main/java/es/lost2found/chatUI/ChatActivity.java
@@ -1,8 +1,6 @@
-package es.lost2found.homeUI;
+package es.lost2found.chatUI;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -19,16 +17,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 import es.lost2found.R;
-import es.lost2found.chatUI.ChatActivity;
+import es.lost2found.homeUI.Announce;
 
-public class HomeActivity extends AppCompatActivity {
+public class ChatActivity extends AppCompatActivity {
 
     private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_chat);
 
         Toolbar tb = findViewById(R.id.toolbar);
         setSupportActionBar(tb);
@@ -39,7 +37,7 @@ public class HomeActivity extends AppCompatActivity {
 
         NavigationView navView = findViewById(R.id.nav_view);
 
-        final Intent chat = new Intent(this, ChatActivity.class);
+        // Final intent chat = new Intent(this, seekerActivity.class)
 
         navView.setNavigationItemSelectedListener(
                 new NavigationView.OnNavigationItemSelectedListener() {
@@ -50,45 +48,20 @@ public class HomeActivity extends AppCompatActivity {
 
                         // Add code here to update the UI based on the item selected
 
-                        if(menuItem.getItemId()== R.id.nav_chat) {
-                            startActivity(chat);
-                        }
+                        // if(menuItem.getItemId()== R.id.nav_search) {
+                        // startActivity(buscar);
+                        //}
 
                         return true;
                     }
                 }
         );
-        /*
-        mDrawerLayout.addDrawerListener(
-                new DrawerLayout.DrawerListener() {
-                    @Override
-                    public void onDrawerSlide(@NonNull View drawerView, float slideOffset) {
-
-                    }
-
-                    @Override
-                    public void onDrawerOpened(@NonNull View drawerView) {
-
-                    }
-
-                    @Override
-                    public void onDrawerClosed(@NonNull View drawerView) {
-
-                    }
-
-                    @Override
-                    public void onDrawerStateChanged(int newState) {
-
-                    }
-                }
-        );
-        */
 
         // In this example we fill announceList with a function fill_with_data(), in the future we'll do it with the database info
-        List<Announce> announceList = fill_with_data();
+        List<Chat> listChat = fill_with_data();
 
-        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.announce_recyclerview);
-        RecyclerViewAdapter adapter = new RecyclerViewAdapter(announceList, getApplication());
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.chat_recyclerview);
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(listChat, getApplication());
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
@@ -96,21 +69,21 @@ public class HomeActivity extends AppCompatActivity {
         RecyclerView.ItemAnimator itemAnimator = new DefaultItemAnimator();
         itemAnimator.setAddDuration(1000);
         itemAnimator.setRemoveDuration(1000);
-        /*recyclerView.setItemAnimator(itemAnimator);*/
+    /*recyclerView.setItemAnimator(itemAnimator);*/
     }
 
-    public List<Announce> fill_with_data() {
+    public List<Chat> fill_with_data() {
 
-        List<Announce> announce = new ArrayList<>();
+        List<Chat> chat = new ArrayList<>();
 
-        announce.add(new Announce("Anuncio1", "Descripcion1", R.drawable.ic_phone_android));
-        announce.add(new Announce("Anuncio2", "Descripcion2", R.drawable.ic_phone_android));
-        announce.add(new Announce("Anuncio3", "Descripcion3", R.drawable.ic_phone_android));
-        announce.add(new Announce("Anuncio4", "Descripcion4", R.drawable.ic_phone_android));
-        announce.add(new Announce("Anuncio5", "Descripcion5", R.drawable.ic_phone_android));
-        announce.add(new Announce("Anuncio6", "Descripcion6", R.drawable.ic_phone_android));
+        chat.add(new Chat("Chat1", "Mensaje1", R.drawable.ic_chaticon));
+        chat.add(new Chat("Chat2", "Mensaje2", R.drawable.ic_chaticon));
+        chat.add(new Chat("Chat3", "Mensaje3", R.drawable.ic_chaticon));
+        chat.add(new Chat("Chat4", "Mensaje4", R.drawable.ic_chaticon));
+        chat.add(new Chat("Chat5", "Mensaje5", R.drawable.ic_chaticon));
+        chat.add(new Chat("Chat6", "Mensaje6", R.drawable.ic_chaticon));
 
-        return announce;
+        return chat;
     }
 
     @Override
@@ -127,7 +100,7 @@ public class HomeActivity extends AppCompatActivity {
      *
      * @param view
      */
-    public void home(View view) {
+    public void chat(View view) {
         /*EditText editText = (EditText) findViewById(R.id.email);
         String name = editText.getText().toString();
 
@@ -155,4 +128,5 @@ public class HomeActivity extends AppCompatActivity {
         //else
         //new RegisterDB().execute(email, pass, name, role);*/
     }
+
 }
--- app/src/main/java/es/lost2found/chatUI/RecyclerViewAdapter.java
+++ app/src/main/java/es/lost2found/chatUI/RecyclerViewAdapter.java
@@ -1,33 +1,31 @@
-package es.lost2found.homeUI;
-
+package es.lost2found.chatUI;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 
 import java.util.Collections;
 import java.util.List;
 
 import es.lost2found.R;
+import es.lost2found.homeUI.View_Holder;
 
-public class RecyclerViewAdapter extends RecyclerView.Adapter<View_Holder> {
-    List<Announce> listAnnounce = Collections.emptyList();
+public class RecyclerViewAdapter extends RecyclerView.Adapter<ChatView_Holder> {
+    List<Chat> listChat = Collections.emptyList();
     Context context;
 
-    public RecyclerViewAdapter(List<Announce> listAnnounce, Context context) {
-        this.listAnnounce = listAnnounce;
+    public RecyclerViewAdapter(List<Chat> listChat, Context context) {
+        this.listChat = listChat;
         this.context = context;
     }
 
     @Override
-    public View_Holder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public ChatView_Holder onCreateViewHolder(ViewGroup parent, int viewType) {
         // Inflate the layout and initialize the View Holder
-        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_annonuce, parent, false);
-        View_Holder holder = new View_Holder(v);
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_chat, parent, false);
+        ChatView_Holder holder = new ChatView_Holder(v);
 
         /*
         holder.itemView.setOnClickListener(new View.OnClickListener() {
@@ -53,18 +51,18 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<View_Holder> {
     }
 
     @Override
-    public void onBindViewHolder(View_Holder holder, int position) {
+    public void onBindViewHolder(ChatView_Holder holder, int position) {
         // Use the provided ChatView_Holder on the onCreateViewHolder method to populate the current row on the RecycleView
-        holder.title.setText(listAnnounce.get(position).title);
-        holder.description.setText(listAnnounce.get(position).description);
-        holder.imageView.setImageResource(listAnnounce.get(position).imageId);
+        holder.chattitle.setText(listChat.get(position).chattitle);
+        holder.lastmsg.setText(listChat.get(position).lastmsg);
+        holder.chaticon.setImageResource(listChat.get(position).chaticon);
 
         //animate(holder);
     }
 
     @Override
     public int getItemCount() {
-        return listAnnounce.size();
+        return listChat.size();
     }
 
     @Override
@@ -73,20 +71,16 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<View_Holder> {
     }
 
     // Insert a new item (announce) to the RecyclerView on a predefined position
-    public void insert(int position, Announce annonuce) {
-        listAnnounce.add(position, annonuce);
+    public void insert(int position, Chat chat) {
+        listChat.add(position, chat);
         notifyItemInserted(position);
     }
 
     // Remove a RecyclerView item containing a specified announce Object
-    public void remove(Announce announce) {
-        int position = listAnnounce.indexOf(announce);
-        listAnnounce.remove(position);
+    public void remove(Chat chat) {
+        int position = listChat.indexOf(chat);
+        listChat.remove(position);
         notifyItemRemoved(position);
     }
 
-    /*public void animate(RecyclerView.ViewHolder viewHolder) {
-        final Animation animAnticipateOvershoot = AnimationUtils.loadAnimation(context, R.anim.bounce_interpolator);
-        viewHolder.itemView.setAnimation(animAnticipateOvershoot);
-    }*/
 }
